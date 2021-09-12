ccdd5d21901db5bcede756caaba9a4f1ffcd2cab
himangshu06 <76275772+himangshu06@users.noreply.github.com>
2021-09-12 11:44:29 +0530

    Add files via upload

---
+++ load.js
@@ -0,0 +1,5 @@
+setTimeout(function(){ 
+    console.log("Loading")
+    window.location.replace="index.html";
+    window.location="index.html";
+}, 12000);
---
+++ player.js
@@ -0,0 +1,7 @@
+UserPlayer = input;
+function login(){
+    if (UserPlayer != ""){
+        window.location.replace="load.html";
+        window.location="load.html";        
+    }
+}
